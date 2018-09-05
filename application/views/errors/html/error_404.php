04f80864370a6cd2270e1c6af8897f0189b25c4a
dev1 <dev@octopuscart.com>
2018-09-05 14:29:11 +0530

    add clients images

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -10,14 +10,16 @@ include APPPATH.'/views/layout/header.php';
 <!-- Content -->
 <div id="content"> 
     <!-- Tesm Text -->
-    <section class="error-page text-center pad-t-b-130">
+    <section class="error-page text-center pad-t-b-130 banner-w3">
         <div class="container "> 
-            <!-- Heading -->
-            <h1>404</h1>
-            <p>We're sorry, the page you requested cannot be found.<br>
-                You can go back to</p>
-            <hr class="dotted">
-            <a href="/" class="btn btn-inverse">BACK TO HOME</a>
+            <div class="latest-w3">
+                <!-- Heading -->
+                <h1 style="    font-size: 115px;">404</h1>
+                <p style="    font-size: 20px;">We're sorry, the page you requested cannot be found.<br>
+                    You can go back to</p>
+                <hr class="dotted">
+                <a href="/"  style="    font-size: 20px;" class="btn btn-primary">BACK TO HOME</a>
+            </div>
         </div>
     </section>
 </div>
