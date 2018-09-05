325c94ebebefb7697a458767e9dadb1e022ff579
dev1 <dev@octopuscart.com>
2018-09-05 07:30:07 +0530

    changes in seo

--- application/config/seo_config.php
+++ application/config/seo_config.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['seo_title'] = 'Class Apart Store';
-$config['seo_desc'] = 'Online Shoping Store';
-$config['seo_imgurl'] = base_url().'assets/images/logo73.jpg';
-$config['seo_keywords'] = "Class Apart Store, Online Shoping, Online Class Apart";
+$config['seo_title'] = "John's Perfect Fashion";
+$config['seo_desc'] = 'Hong Kong has a worldwide reputation as being a centre of world-class tailoring. Perfect Fashion is one of the leading tailoring companies in Hong Kong, and our fashion consultants will assist you in choosing your style.';
+$config['seo_imgurl'] = base_url().'assets/images/logo1.png';
+$config['seo_keywords'] = "John's Perfect Fashion, Suit Online Shoping in Hong Kong, Hong Kong Tailor, Bespoke Tailor In Hong Kong";
