ff5a4503685a8afaec1d9676e0e750ce4adce230
dev1 <dev@octopuscart.com>
2018-10-29 12:41:50 +0530

    changes

--- application/views/home.php
+++ application/views/home.php
@@ -3,16 +3,16 @@ $this->load->view('layout/header');
 ?>
 
 <script>
-            $(function () {
-                $("#slider").responsiveSlides({
-                    auto: true,
-                    nav: true,
-                    speed: 500,
-                    namespace: "callbacks",
-                    pager: true,
-                });
-            });
-        </script>
+    $(function () {
+        $("#slider").responsiveSlides({
+            auto: true,
+            nav: true,
+            speed: 500,
+            namespace: "callbacks",
+            pager: true,
+        });
+    });
+</script>
 
 <!--slider part-->
 <div class="banner-w3">
@@ -44,11 +44,11 @@ $this->load->view('layout/header');
     <link href="<?php echo base_url(); ?>assets/theme/css/coreSlider.css" rel="stylesheet" type="text/css">
     <script src="<?php echo base_url(); ?>assets/theme/js/coreSlider.js"></script>
     <script>
-        $('#example1').coreSlider({
-            pauseOnHover: false,
-            interval: 3000,
-            controlNavEnabled: true
-        });</script>
+    $('#example1').coreSlider({
+        pauseOnHover: false,
+        interval: 3000,
+        controlNavEnabled: true
+    });</script>
 
 </div>	
 <!--end of slider part-->
@@ -217,75 +217,62 @@ $this->load->view('layout/header');
     </div>
 
     <?php
-
     ?>
 </div>
 <!--content-->
 <hr/>
 
+<?php
+$clients = array(
+    '1' => array('name' => 'Dean H', 'position' => 'TripAdvisor', 'review' => "I met Malik four years ago while in Hong Kong. The quality of New Super Fit Fashion is far superior than other more well-known and more expensive tailors. Malik stands by his clothing and I’ll be coming back as well as recommending him to others.
+
+"),
+    '2' => array('name' => 'Robert P', 'position' => 'TripAdvisor', 'review' => "I went to Superfit fashions three times in the last 8 years or so for shirts. When I came for the second time after some 5 years they still had my measurements. I outgrew them a little (sigh)
+Very good quality. It was delivered at my hotel within the agreed time and I can honestly say the quality is extremely good. Maybe not the cheapest tailor (although I haven't visited another one but from hearsay) but after years the shirts are still great!!
+Can't wait to go back to HKG. I will DEFINATELY stop at their shop again!
+Compliments!!"),
+);
+?>
+
 <!--clients area-->
-<div class="latest-w3 testimonials" style="background: url(<?php echo base_url(); ?>assets/theme/images/backclient.jpg);    background-size: cover;">
+<div class="latest-w3 testimonials" style="    padding: 2em 0 0em;background: url(<?php echo base_url(); ?>assets/theme/images/backclient.jpg);    background-size: cover;">
     <h2>Testimonials</h2>
-<div id="myCarousel" class="carousel slide" data-ride="carousel">
-	<!-- Carousel indicators -->
-	<ol class="carousel-indicators">
-		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-		<li data-target="#myCarousel" data-slide-to="1"></li>
-		<li data-target="#myCarousel" data-slide-to="2"></li>
-	</ol>   
-	<!-- Wrapper for carousel items -->
-	<div class="carousel-inner">		
-		<div class="item carousel-item active">
-			<div class="img-box"><img src="<?php echo base_url(); ?>assets/theme/images/user.png" alt=""></div>
-			<p class="testimonial">Phasellus vitae suscipit justo. Mauris pharetra feugiat ante id lacinia. Etiam faucibus mauris id tempor egestas. Duis luctus turpis at accumsan tincidunt. Phasellus risus risus, volutpat vel tellus ac, tincidunt fringilla massa. Etiam hendrerit dolor eget rutrum.</p>
-			<p class="overview"><b>Michael Holz</b>Seo Analyst at <a href="#">OsCorp Tech.</a></p>
-			<div class="star-rating">
-				<ul class="list-inline">
-					<li class="list-inline-item"><i class="fa fa-star"></i></li>
-					<li class="list-inline-item"><i class="fa fa-star"></i></li>
-					<li class="list-inline-item"><i class="fa fa-star"></i></li>
-					<li class="list-inline-item"><i class="fa fa-star"></i></li>
-					<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
-				</ul>
-			</div>
-		</div>
-		<div class="item carousel-item">
-			<div class="img-box"><img src="/examples/images/clients/1.jpg" alt=""></div>
-			<p class="testimonial">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante. Vestibulum idac nisl bibendum scelerisque non non purus. Suspendisse varius nibh non aliquet.</p>
-			<p class="overview"><b>Paula Wilson</b>Media Analyst at <a href="#">SkyNet Inc.</a></p>
-			<div class="star-rating">
-				<ul class="list-inline">
-					<li class="list-inline-item"><i class="fa fa-star"></i></li>
-					<li class="list-inline-item"><i class="fa fa-star"></i></li>
-					<li class="list-inline-item"><i class="fa fa-star"></i></li>
-					<li class="list-inline-item"><i class="fa fa-star"></i></li>
-					<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
-				</ul>
-			</div>
-		</div>
-		<div class="item carousel-item">
-			<div class="img-box"><img src="/examples/images/clients/2.jpg" alt=""></div>
-			<p class="testimonial">Vestibulum quis quam ut magna consequat faucibus. Pellentesque eget nisi a mi suscipit tincidunt. Utmtc tempus dictum risus. Pellentesque viverra sagittis quam at mattis. Suspendisse potenti. Aliquam sit amet gravida nibh, facilisis gravida odio. Phasellus auctor velit.</p>
-			<p class="overview"><b>Antonio Moreno</b>Web Developer at <a href="#">Circle Ltd.</a></p>
-			<div class="star-rating">
-				<ul class="list-inline">
-					<li class="list-inline-item"><i class="fa fa-star"></i></li>
-					<li class="list-inline-item"><i class="fa fa-star"></i></li>
-					<li class="list-inline-item"><i class="fa fa-star"></i></li>
-					<li class="list-inline-item"><i class="fa fa-star"></i></li>
-					<li class="list-inline-item"><i class="fa fa-star-half-o"></i></li>
-				</ul>
-			</div>
-		</div>		
-	</div>
-	<!-- Carousel controls -->
-	<a class="carousel-control left carousel-control-prev" href="#myCarousel" data-slide="prev">
-		<i class="fa fa-angle-left"></i>
-	</a>
-	<a class="carousel-control right carousel-control-next" href="#myCarousel" data-slide="next">
-		<i class="fa fa-angle-right"></i>
-	</a>
-</div>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Carousel indicators -->
+        <ol class="carousel-indicators">
+            <?php foreach ($clients as $key => $value) { ?>
+                <li data-target="#myCarouse<?php echo $key; ?>" data-slide-to="<?php echo $key; ?>" class="active"></li>
+            <?php } ?>
+        </ol>   
+        <!-- Wrapper for carousel items -->
+        <div class="carousel-inner">	
+            
+            <?php foreach ($clients as $key => $value) { ?>
+            <div class="item carousel-item <?php echo $key=='1'?'active':'';?>">
+                <!--<div class="img-box"><img src="<?php echo base_url(); ?>assets/theme/images/user.png" alt=""></div>-->
+                <p class="testimonial"><?php echo $value['review'];?></p>
+                <p class="overview"><b><?php echo $value['name'];?></b>Review at <a href="#"><?php echo $value['position'];?></a></p>
+                <div class="star-rating">
+                    <ul class="list-inline">
+                        <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                        <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                        <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                        <li class="list-inline-item"><i class="fa fa-star"></i></li>
+                        <li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+                    </ul>
+                </div>
+            </div>
+            <?php } ?>
+        		
+        </div>
+        <!-- Carousel controls -->
+        <a class="carousel-control left carousel-control-prev" href="#myCarousel" data-slide="prev">
+            <i class="fa fa-angle-left"></i>
+        </a>
+        <a class="carousel-control right carousel-control-next" href="#myCarousel" data-slide="next">
+            <i class="fa fa-angle-right"></i>
+        </a>
+    </div>
 </div>
 <!--end of client area-->
 <?php
